e2b84c9c13f1af7832a7da99353be1fc96a70aaa
arcanaknight1 <33413009+arcanaknight1@users.noreply.github.com>
2018-09-06 22:24:44 -0400

    Added radio button to table tab
    Added a radio input to the table tab & made cosmetic changes to the graph

--- Assignment1.R
+++ Assignment1.R
@@ -14,41 +14,47 @@ cancer <- esoph
 colnames(cancer) <- c("Age_Group", "Alcohol_Use", "Tobacco_Use", "Num_Cases", "Num_Controls")
 pdf(NULL)
 
-#Define UI for app that shows table and plots a bar
+#Define UI for app that shows table and plots a bar chart
 ui <- fluidPage(
   navbarPage("Esophageal Cancer Navigation Bar", 
              theme = shinytheme("journal"),
-             tabPanel("Alcohol",
+             tabPanel("Graph",
                       sidebarLayout(
                         sidebarPanel(
                           checkboxGroupInput("Age_Select",
                                              "Age:",
                                              choices = levels(cancer$Age_Group),
-                                             selected = c("25-34", "35-44"))
+                                             selected = c("25-34", "35-44", "45-54","55-64"))
                           ),
-                        # Output plot
+                        # Output barplot
                         mainPanel(
-                          plotlyOutput("Alcohol")
+                          plotlyOutput("Graph")
                         )
                       )),
              # Data Table
-             tabPanel("Tobacco",
-                      fluidPage(DT::dataTableOutput("Tobacco"))
-             )
+             tabPanel("Table",
+                      sidebarLayout(
+                        sidebarPanel(
+                          radioButtons("Cause_Select",
+                                             "Age:",
+                                             choices = c("Tobacco Use", "Alcohol Use"),
+                                             selected = c("Tobacco Use"))
+                        ),
+                        mainPanel(
+                          fluidRow(DT::dataTableOutput("Table")))
+             ))
   )
 )
 
 # Define server logic
 server <- function(input, output) {
-  output$Alcohol <- renderPlotly({
+  output$Graph <- renderPlotly({
     cancerdata <- subset(cancer, Age_Group %in% input$Age_Select)
-    ggplot(data = cancerdata, aes(x = Alcohol_Use, y = Num_Cases, fill = Age_Group)) + geom_bar(stat = "identity") +
-      xlab("Alcohol Use") + ylab("Number of Cases")
-    #ggplot(data = cancerdata, aes(x = Tobacco_Use, y = Num_Cases, fill = Age_Group)) + geom_bar(stat = "identity") +
-    #  xlab("Tobacco Use") + ylab("Number of Cases")
+    ggplot(data = cancer, aes(x = Alcohol_Use, y = Num_Cases, fill = Age_Group)) + geom_bar(stat = "identity") +
+     labs(title = "Esophogeal Cancer Cases Related to Alcohol", x ="Alcohol Use", y = "Number of Cases", fill = "Age Group")
   })
-  output$Tobacco <- DT::renderDataTable({
-    subset(cancer, Age_Group %in% input$Age_Select, select = c(Age_Group, Alcohol_Use, Tobacco_Use, Num_Cases, Num_Controls))
+  output$Table <- DT::renderDataTable({
+    subset(cancer, select = c(Age_Group, Alcohol_Use, Tobacco_Use, Num_Cases, Num_Controls))
   })
 }
 
